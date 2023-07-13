b96c06bbd64d2b65c8e08fc10c3e13a1472eefb6
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-13 08:19:43 -0700

    modification on 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'using puts, instead of printf'
+ *
+ * Return: always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
