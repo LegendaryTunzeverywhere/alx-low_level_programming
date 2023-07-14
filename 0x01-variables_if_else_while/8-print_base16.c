4dbc74c505684e791f6e5e5051cec9d534ca228b
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-14 08:51:26 -0700

    print base 18

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints hexadecimal base
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+	int a_to_f = 'a';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+
+	while (a_to_f <= 'f')
+	{
+		putchar(a_to_f);
+		a_to_f++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
