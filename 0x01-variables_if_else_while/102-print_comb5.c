f7aa3b119db30105048994a3604a6c1b34927119
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-14 10:43:50 -0700

    unfinished

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints a number pair
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens;
+	int ones;
+	int t;
+	int o;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
+			for (t = '0'; t <= '9'; t++)
+			{
+				for (o = '0'; o <= '9'; o++)
+				{
+					putchar(tens);
+					putchar(ones);
+					putchar(' ');
+					putchar(t);
+					putchar(o);
+
+					if (!((tens == '9' && ones == '8') &&
+								(t == '9' && o == '9')))
