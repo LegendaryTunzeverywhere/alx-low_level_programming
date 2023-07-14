921834ff2ad2563a9297769b70bc7fbb4b0463ab
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-14 09:06:13 -0700

    print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers with commas, using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n')
+
+		return (0);
+}
