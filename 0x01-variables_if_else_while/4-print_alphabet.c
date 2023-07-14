400af551e4d81618091bc25a3fe51fdee18a55d1
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-14 08:08:04 -0700

    print all alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints all alphabet except q and e
+ *
+ * Return: Aleays 0 (Success)
+ */
+int main(void)
+{
+	char = letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+
+	putchar(letter);
+	
+	return (0);
+}
