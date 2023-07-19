7fb097be04ed73d8c0b302152883b53918174126
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-19 06:08:24 -0700

    Third commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10(void) - Entry point
+ *
+ * Description: Print lowercase alphabet 10x
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 10;
+	int w;
+	int c;
+
+	for (w = 0; w < i; w++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		
+		_putchar('\n');
+	}
+}
