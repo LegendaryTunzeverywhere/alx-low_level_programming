e57a4e4013b2bb4acbfbb6601568612a5bf4befc
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-20 09:08:21 -0700

    Add two integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * Description: Two integers added together.
+ * @a: Parameter 1
+ * @b: parameter 2
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
