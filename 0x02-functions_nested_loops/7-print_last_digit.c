cb771f9cb89715197cbbc6a6cc49ecb3225c6fa9
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-20 06:08:02 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * Description: print the last digit of a number.
+ * @r: parameter.
+ *
+ * Return: Always 0
+ */
+int print_last_digit(int r)
+{
+	int ld = r % 10;
+
+	if (ld < 0)
+	{
+		_putchar(-ld + '0');
+		return (-ld);
+	}
+	else
+	{
+		_putchar(ld + '0');
+		return (ld);
+	} 
+	_putchar('\n');
+	return (0);
+}
