1eaf4191d35680923deeda4a54bb256420953900
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-20 15:49:08 -0700

    print 0 to 9 without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ * Description print 0 to 9 except 2 and 4
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		if (n != '2' && n != '4')
+			_putchar(n);
+	}
+	_putchar('\n');
+}
