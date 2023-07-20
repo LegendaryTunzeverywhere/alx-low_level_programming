f26900232c813e5a109742154ca2b4cc6843eca7
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-20 07:02:15 -0700

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,25 @@
 #include "main.h"
 
 /**
- * jack_bauer
+ * jack_bauer - Entry point
+ *
+ * Descrption: Print time.
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	for (hours = '0'; hours < 24; hours++)
+	for (minutes = '0'; minutes < 60; minutes++)
+	{
+		_putchar('0' + hours / 10);
+		_putchar('0' + hours % 10);
+		_putchar(':');
+		_putchar('0' + minutes / 10);
+		_putchar('0' + minutes % 10);
+		_putchar('\n');
+	}
+}
