c388155651f54bdbd5eedfae7d4f21e5e118470e
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-24 12:47:20 -0700

    function to swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,13 +4,16 @@
  * swap_int - Entry point
  *
  * Description: Swap the values of a and b.
- * @a: pointer 1 
+ * @a: pointer 1
  * @b: pointer 2
  *
  * Return: Always 0
  */
 void swap_int(int *a, int *b)
 {
+	int temp;
+
+	temp = *a;
 	*a = *b;
-	*b = *a;
+	*b = temp;
 }
