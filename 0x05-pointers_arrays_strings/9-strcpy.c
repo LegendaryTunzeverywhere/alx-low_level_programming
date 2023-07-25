67039ade82bb8eaa1105254b75357249ece0a0f7
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-24 18:28:40 -0700

    dereferencing

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: string to print
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * _strcpy - Entry point
+ *
+ * Description: copies the string pointed to by source.
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
+ */
+char *strcpy(char *dest, char *src)
+{
+	 while (*src)
+	 {
+		 *dest = *src;
+		 src++;
+		 dest++
+	 }
+	 return (dest);
+}
