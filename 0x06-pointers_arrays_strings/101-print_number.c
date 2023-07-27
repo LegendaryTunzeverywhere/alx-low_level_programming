cd4cff12055e3eebbaf5646d39a4d458afa0fde9
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-27 13:48:12 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ *
+ * Description: print an integer, without using long, arrays or pointers.
+ * @n: integer to print
+ * Return: Always 0
+ */
+void print_number(int n)
+{
+	unsigned int num1;
+
+	num1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num1 = -n;
+	}
+
+	if (num1 / 10 != 0)
+	{
+		print_number(num1 / 10);
+	}
+	_putchar((num1 % 10) + '0');
+}	
