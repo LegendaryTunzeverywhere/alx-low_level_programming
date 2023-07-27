fba97114fc42ea1d0b0dc791b7cc0e8bfc2b1d14
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-27 09:08:24 -0700

    capitalize

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,7 +11,7 @@ char *string_toupper(char *s)
 {
 	int a = 0;
 
-	for (a = 0; s[!a] = '\0'; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	{
 		if (s[a] >= 'a' && s[a] <= 'z')
 			s[a] = s[a] - 'a' + 'A';
