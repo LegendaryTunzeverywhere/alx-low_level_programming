3bfe7a49f4ebf4f174bfc041080446ac596ed1a8
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-27 08:59:54 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - Entry point
+ *
+ * Description: capitalize all letters in string.
+ * @s: string to manipulate
+ * Return: string with all lettersin capital letter
+ */
+char *string_toupper(char *s)
+{
+	int a = 0;
+
+	for (a = 0; s[!a] = '\0'; a++)
+	{
+		if (s[a] >= 'a' && s[a] <= 'z')
+			s[a] = s[a] - 'a' + 'A';
+	}
+	return (s);
+}
