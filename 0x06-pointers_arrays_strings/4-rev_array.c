918478352f6572acbe6ce06956ce7138b70544a3
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-27 03:25:56 -0700

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ *
+ * Description: Reverses the contents of an integers
+ * @a: array
+ * @n: number of elements in array
+ */
+void reverse_array(int *a, int n)
+{
+	int temp, b = 0;
+	int c = n - 1;
+
+	while (b < c)
+	{
+		temp = *(a + b);
+		*(a + b) = *(a + c);
+		*(a + c) = temp;
+		b++; c--;
+	}
+}
