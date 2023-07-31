0b5daac9af62401fe6a16f2fc56d2abc37ee0af5
Adewale Christopher Okunade <princecph4@gmail.com>
2023-07-31 08:49:09 -0700

    added execution

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,13 +12,13 @@ unsigned int _strspn(char *s, char *accept)
 	int b;
 	unsigned int len;
 
-	len = 0
+	len = 0;
 	for (a = 0; s[a] != '\0'; a++)
 	{
-		for (b = 0; accept[b] !- '\0' && accept[b] != s[a]; b++)
+		for (b = 0; accept[b] != '\0' && accept[b] != s[a]; b++)
 			;
 		if (s[a] == accept[b])
-			len;
+			len++;
 		if (accept[b] == '\0')
 			return (len);
 	}
