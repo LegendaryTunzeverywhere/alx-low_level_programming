3ffcfaf468b3cddfeaa070a79cbf3665a76377b0
Adewale Christopher Okunade <princecph4@gmail.com>
2023-08-05 08:11:05 -0700

    updated version

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,28 @@
+#include "main.h"
 
+/**
+ * _atoi - Entry point
+ * @s: string to print
+ *
+ * Description: to convert a string into an integer.
+ * Return: Always 0
+ */
+int _atoi(char *s)
+{
+	int a = 1, i = 0;
+	unsigned int b = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			a *= -1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		b = (b * 10) + (s[i] - '0');
+		i++;
+	}
+	b *= a;
+	return (b);
+}
