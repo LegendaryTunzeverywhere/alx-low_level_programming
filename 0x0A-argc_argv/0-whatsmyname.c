d25f932fe7dc8a7cd510ed349ed09a5b7c9fd66a
Adewale Christopher Okunade <princecph4@gmail.com>
2023-08-06 08:45:14 -0700

    initail commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of argument
+ * @argv: array of argument
+ *
+ * Description: prints the name of the program.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,6 @@
+# This repository contains the following:
+
+* Argv - Argument Vector
+* Argc - Argument Count
+* Main.h - Prototypes
+* _putchar - Allows printing
