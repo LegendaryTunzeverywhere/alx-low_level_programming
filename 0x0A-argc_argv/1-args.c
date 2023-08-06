e44cb87fda8c2d394622ae41c040985d5969373c
Adewale Christopher Okunade <princecph4@gmail.com>
2023-08-06 09:21:06 -0700

    modified

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,10 +7,10 @@
  * @argv: pointer to arrayof argument
  *
  * Description: prints the number of arguments passed to program.
- * Rreturn: Always 0 (Success).
+ * Return: Always 0 (Success).
  */
-int main(int argc, char *argv[] __attribute__ ((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
