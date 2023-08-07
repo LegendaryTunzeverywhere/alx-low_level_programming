d48bebeb72613925d5895f3deed5a219eff9e1a4
Adewale Christopher Okunade <princecph4@gmail.com>
2023-08-07 13:20:36 -0700

    modified

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *str;
 	unsigned int a;
 
-	if = (size == 0)
+	if (size == 0)
 		return (NULL);
 	str = malloc(sizeof(char) * size);
 	if (str == NULL)
