4689697a50ca522e4c33e65380fcbf575667b3be
Adewale Okunade Christopher <princecph4@gmail.com>
2023-08-10 18:35:43 +0100

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - Entry point
+ * @nmemb - array
+ * @size - size of bytes
+ *
+ * Description: allocates memory for an array using malloc.
+ * Return: NULL if size of nmemb is 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int a;
+	char *c;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	c = malloc(nmemb * size);
+	if (c == NULL)
+		return (NULL);
+	for (a = 0; a < nmemb * size; a++)
+		*(c + a) = 0;
+	return ((void *)c);
+}
