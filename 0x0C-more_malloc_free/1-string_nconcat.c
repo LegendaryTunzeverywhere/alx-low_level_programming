28d4d91f0882b0cd21f95f66e9791d89013c9304
Adewale Okunade Christopher <princecph4@gmail.com>
2023-08-10 17:15:27 +0100

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,20 +16,26 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = (s1 == NULL) ? 0 : strlen(s1);
-	unsigned int len2 = (s2 == NULL) ? 0 : strlen(s2);
-	unsigned int total_len = len1 + ((n >= len2) ? len2 : n) + 1;
-
-	char *c = malloc(total_len);
-
-	if (c == NULL)
-		return (NULL);
+	unsigned int len1 = 0, len2 = 0, total_len = 0;
+	char *c = NULL;
 
 	if (s1 != NULL)
-		strncpy(c, s1, len1);
+		len1 =strlen(s1);
+	if (s2 != NULL)
+		len2 = strlen(s2);
+
+	if (n >= len2)
+		n = len2;
+
+	total_len = len1 + n + 1;
+
+	c = malloc(total_len);
+
+	if (s1 != NULL)
+		strcpy(c, s1);
 
 	if (s2 != NULL && n > 0)
-		strncat(c + len1, s2, n);
+		strncat(c, s2, n);
 
 	c[total_len - 1] = '\0';
 
