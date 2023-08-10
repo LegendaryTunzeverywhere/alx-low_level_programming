c370e8947245a31b5969b2915ac02d49bbf04905
Adewale Okunade Christopher <princecph4@gmail.com>
2023-08-10 16:24:36 +0100

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,7 +16,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, total_len;
+	unsigned int len1 = 0, len2 = 0, total_len = 0;
 	char *c;
 
 	if (s1 == NULL)
