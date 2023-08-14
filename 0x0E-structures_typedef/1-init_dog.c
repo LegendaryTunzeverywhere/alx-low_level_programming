08155b58db3d6dd3841d06c6223be3cdb0414854
Adewale Okunade Christopher <princecph4@gmail.com>
2023-08-14 15:39:49 +0100

    Task12

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Entry point
+ * @d: a pointer
+ * @name: pointer to name of dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
+ * Description: Initialize a variable of type struct dog.
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float agge, char *owner)
+{
+	if (d == NULL)
+		;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
