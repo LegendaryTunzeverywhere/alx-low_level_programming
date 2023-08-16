e45c82c367db99f5fc83669a971feb625a7ec8a4
Adewale Christopher Okunade <princecph4@gmail.com>
2023-08-16 08:28:35 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Entry point
+ * @name: pointer to char
+ * @f: pointer to function, returns nothing.
+ *
+ * Description: prints a name
+ * Return: nothing
+ */
+void print_name(char *name, void (*f) (char *))
+{
+	if (name != NULL && *name != '\0' && f != NULL)
+		f(name);
+}
