c3a24f4b9b064b0e6819eb33c988440604f37738
Adewale Okunade Christopher <princecph4@gmail.com>
2023-08-24 22:36:12 +0100

    Task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,53 @@
+#include "lists.h"
+
+/**
+ * _strlen - Entry point
+ * @s: string to acquire length.
+ *
+ * Description: get length of string
+ * Return: length of the string.
+ */
+int _strlen(const char *str)
+{
+	int len;
+
+	for (len = 0; str[len]; len++)
+		;
+	return (len);
+}
+
+/**
+ * add_node - Entry node
+ * @head: current position in the list
+ * @str: string to be added to the list
+ *
+ * Description: add new nodes to the list
+ * Return: pointer to current place in the list.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int a, len;
+	char *filler;
+	list_t *new_node;
+
+	if (str == NULL || head == NULL)
+		return (NULL);
+	len = _strlen(str);
+	new_node = *head;
+	filler = malloc((len + 1) * sizeof(char));
+	if (filler == NULL)
+		return (NULL);
+	for (a = 0; str[a]; a++)
+		filler[a] = str[a];
+	new_node = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(content);
+		return (NULL);
+	}
+	new_node->str = filler;
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new;
+	return (new_node);
+}
