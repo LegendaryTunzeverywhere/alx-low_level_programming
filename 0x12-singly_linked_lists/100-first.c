840ece70f7358e09952048417f4c3bd810fc49ca
Adewale Christopher Okunade <princecph4@gmail.com>
2023-08-24 16:56:47 -0700

    advance task 0

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,6 +4,7 @@
  * a - execute this function before main function
  */
 void a(void) __attribute__ ((constructor));
+void a(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n);
