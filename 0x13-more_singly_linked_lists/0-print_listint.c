1c3fc4d526128b7b771ca1b9c35c4ac85c9bbfc5
Adewale Okunade Christopher <princecph4@gmail.com>
2023-08-29 12:44:49 +0100

    Betty Compliance

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,7 +5,7 @@
  * @h: singly linked list to be printed
  *
  * Description: Prints all the element of listint_t list
- * Retrun: the number of nodes.
+ * Return: the number of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
