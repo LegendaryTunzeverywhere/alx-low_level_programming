3911a8310f20bd5f60711255a9c16965c51f0763
Adewale Okunade Christopher <princecph4@gmail.com>
2023-08-29 14:50:55 +0100

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,16 +19,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
-	if (index == 0)
+	if (idx == 0)
 	{
 		newNode->next = place;
 		*head = newNode;
 		return (*head);
 	}
-	while (index > 1)
+	while (idx > 1)
 	{
 		place = place->next;
-		index--;
+		idx--;
 		if (!place)
 		{
 			free(newNode);
