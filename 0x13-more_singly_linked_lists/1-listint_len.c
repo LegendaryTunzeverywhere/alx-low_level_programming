86f4254f2f94cbbc0cf0b83c7795cb57fc4fd72b
Adewale Okunade Christopher <princecph4@gmail.com>
2023-08-29 12:49:53 +0100

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - Entry point
+ * @h: singly linked to list
+ *
+ * Description: Returns the number of elements 
+ * in a linked listint_t list
+ *
+ * Return: number of elements in list.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t a;
+
+	for (a = 0; h; a++)
+		h = h->next;
+	return (a);
+}
