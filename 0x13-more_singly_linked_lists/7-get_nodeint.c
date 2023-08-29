0c223754c52b8e11c58c3b898b3e74621c4f0a0f
Adewale Christopher Okunade <princecph4@gmail.com>
2023-08-29 06:11:39 -0700

    Task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Entry point
+ * @head: start of the list.
+ * @index: index of node.
+ *
+ * Description: returns the nth node of the linked list.
+ * Return: the nth node of the list.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int a;
+
+	a = 0;
+	if (a < index)
+	{
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
+		a++;
+	}
+	return (head);
+}
