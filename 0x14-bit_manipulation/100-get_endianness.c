8d39891178a4209b5df1f66841fde5a128c6c6c7
Adewale Okunade Christopher <princecph4@gmail.com>
2023-08-31 23:13:45 +0100

    Left for testing on your pc

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - Entry point
+ *
+ * Description: checks for endianness.
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+	int a;
+	char *b;
+
+	a = 1;
+	b = (char *)&a;
+	return (*b);
+}
