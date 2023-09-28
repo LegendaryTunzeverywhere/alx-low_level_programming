58f5323fc046d07bde34c50986a86d42984f56bc
Legendarytunzevereywhere <princecph4@gmail.com>
2023-09-28 07:57:54 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,12 +15,12 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (total);
-	for (a = 0; b[a] != '0'; ++a)
+	for (a = 0; b[a] != '\0'; a++)
 	{
 		if (b[a] != '0' && b[a] != '1')
 			return (0);
 	}
-	for (a = 0; b[a] != '\0'; ++a)
+	for (a = 0; b[a] != '\0'; a++)
 	{
 		total <<= 1;
 		if (b[a] == '1')
