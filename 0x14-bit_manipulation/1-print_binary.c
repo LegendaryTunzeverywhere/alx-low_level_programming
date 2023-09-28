f6dd70c10a3eb3a4e9487bd812db66db0d10e5bc
Legendarytunzevereywhere <princecph4@gmail.com>
2023-09-28 08:21:32 -0700

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the charcater c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * _expon - Entry point
+ * @base: base 2 to be converted.
+ * @power: number of exponent
+ *
+ * Description: calculate base raise to power.
+ * Return: value of the equation.
+ */
+unsigned long int _expon(unsigned int base, unsigned int power)
+{
+	unsigned long int i;
+	unsigned int num;
+
+	i = 1;
+	for (num = 1; num <= power; ++num)
+		i *= base;
+	return (i);
+}
+
+/**
+ * print_binary - Entry point
+ * @n: number to print as a binary.
+ *
+ * Description: prints the representation of a number.
+ * Return: nothing.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int a, b;
+	char d;
+
+	d = 0;
+	a = _expon(2, sizeof(unsigned long int) * 8 - 1);
+	while (a != 0)
+	{
+		b = n & a;
+		if (b == a)
+		{
+			 d = 1;
+			 _putchar('1');
+		}
+		else if (d == 1 || a == 1)
+		{
+			_putchar ('0');
+		}
+		a >>= 1;
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+unsigned long int _expon(unsigned int base, unsigned int power);
+int _putchar(char c);
 
 #endif
