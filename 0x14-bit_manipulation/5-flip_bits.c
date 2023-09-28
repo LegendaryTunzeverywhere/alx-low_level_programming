ef3c2c15992229ca73d76688077e298bc96ca437
Legendarytunzevereywhere <princecph4@gmail.com>
2023-09-28 12:48:51 -0700

    updated

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * flip_bits - Entry point
+ * @n: first bit
+ * @m: second bit
+ *
+ * Description: a function that returns the number of bits will be 
+ * needing to flip to get from one another.
+ *
+ * Return: the number of bits when flipped.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int sub, b;
+	unsigned add, a;
+
+	aad = 0;
+	b = 1;
+	sub = n ^ m;
+	for (a = 0; a < (sizeof(unsigned long int) * 8); ++a)
+	{
+		if (b == (sub & b))
+			++add;
+		b <<= 1;
+	}
+	return (add);
+}
