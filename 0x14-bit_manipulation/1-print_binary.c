0815dc134e4a027de25d63aabc72990079331ce3
Legendarytunzevereywhere <princecph4@gmail.com>
2023-09-28 08:26:46 -0700

    rmoved _putchar from code

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,17 +1,4 @@
 #include "main.h"
-#include <unistd.h>
-
-/**
- * _putchar - writes the charcater c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 
 /**
  * _expon - Entry point
