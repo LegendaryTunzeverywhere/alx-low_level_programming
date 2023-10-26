1348e4c6990061ac7e607872f0f325051741850b
Adewale Okunade <princecph4@gmail.com>
2023-10-26 14:39:04 -0700

    Task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - Entry point
+ * @n: number of index to set
+ * @index: starts from 0.
+ *
+ * Description: sets the value of a bit to 1 at agiven index.
+ * Return: 1 if sucess, -1 if error.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int a;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	a = 1 << index;
+	*n = *n | a;
+	return (1);
+}
