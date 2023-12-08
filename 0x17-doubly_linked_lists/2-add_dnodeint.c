1f8b5806662d1886fee7a2b845607f79d40311d6
LegendaryTunzeverywhere <princecph4@gmail.com>
2023-12-07 16:34:23 -0800

    betty compliance

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
-/** add_dnodeint - Entry point
+/** 
+ * add_dnodeint - Entry point
  * @head: pointer to head pointer of the list
  * @n: value of the new node.
  *
- * Description: function that adds a new node at the beginning of a linked list.
+ * Description: function that adds a new node at the beginning of a list.
  * Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
