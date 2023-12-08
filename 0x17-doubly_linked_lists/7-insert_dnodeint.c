f8c38059775338aa8d93c647d9e49ed345a97f0f
Adewale Okunade Christopher <princecph4@gmail.com>
2023-12-08 01:33:42 +0000

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,55 +1,60 @@
 #include "lists.h"
 
 /**
+ * insert_node - Entry point
+ * @temp: ptr to nth position node in doubly linked list
+ * @n: node data
+ *
+ * Description: insert a node at given index
+ * Return: address of inserted node
+ */
+dlistint_t *insert_node(dlistint_t *tmp, int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(struct dlistint_s));
+	if (!new)
+		return (NULL);
+	new->n = n;
+
+	new->next = temp;
+	new->prev = temp->prev;
+	temp->prev->next = new;
+	temp->prev = new;
+
+	return (new);
+}
+
+/**
  * insert_dnodeint_at_index - Entry point
- * @head: pointer to the head of the list.
+ * @h: pointer to the head of the list.
  * @idx: index to add new node, starts from 0.
  * @n: value of the new node.
  *
  * Description: function that inserts a new node at a given position.
  * Return: the address of the new node, or NULL if it failed.
  */
-dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int add;
-	dlistint_t *temp, *new, *temp_prev;
-
-	if (head == NULL && idx > 0)
-	return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n, new->prev = new->next = NULL;
+	dlistint_t *temp;
 
 	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (!h)
+		return (NULL);
+
+	temp = *h;
+	while ((idx != 0) && (temp->next))
 	{
-		if (*head)
-	{
-		new->next = *head;
-		(*head)->prev = new, *head = new;
-	}
-		else
-			*head = new;
-		return (new);
-	}
-	add = 1, temp = (*head)->next;
-	while (temp)
-	{
-		if (idx == add)
-		{
-			temp->prev->next = new, new->prev = temp->prev;
-			new->next = temp, temp->prev = new;
-			return (new);
-		}
-		++add;
-		temp_prev = temp;
+		idx -= 1;
 		temp = temp->next;
+		if (idx == 0)
+			return (insert_node(temp, n));
 	}
-	if (temp == NULL && add == idx)
-	{
-		temp_prev->next = new, new->prev = temp_prev;
-		return (new);
-	}
-	free(new);
+
+	if (idx == 1)
+		return (add_dnodeint_end(h, n));
 	return (NULL);
 }
+
+
