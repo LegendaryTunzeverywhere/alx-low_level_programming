39ee444135b503d6575e6c67e0102b529f46a69b
Adewale Okunade Christopher <princecph4@gmail.com>
2023-12-30 23:51:16 +0000

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,28 +11,47 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long idx, size;
-	hash_node_t *new_node;
-	
-	if (ht == NULL || key == NULL || value == NULL)
-		return (0);
-	
-	size = ht->size;
-	idx = key_idx((const unsigned char *)key, size);
+	hash_node_t *newNode, *temp;
+	unsigned long int idx;
 
-	if (ht->array[idx] != NULL && strcmp(ht->array[idx]->key, key) == 0)
+	if (ht == NULL)
+		return (0);
+	idx = (hash_djb2((const unsigned char *)key) % (ht->size));
+	if (key == NULL || value == NULL || (strcmp(key, "") == 0))
+		return (0);
+	if (ht->array[idx] == NULL)
 	{
-		ht->array[idx]->value = strdup(value);
+		newNode = malloc(sizeof(hash_node_t));
+		if (newNode == NULL)
+			return (0);
+		ht->array[idx] = newNode;
+		newNode->key = (strdup(key));
+		newNode->value = (strdup(value));
+		newNode->next = NULL;
 		return (1);
 	}
-	new_node = malloc(sizeof(hash_node_t));
-	
-	if (new_node == NULL)
-		return (0);
-	
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = ht->array[idx];
-	ht->array[idx] = new_node;
-	return (1);
+	temp = ht->array[idx];
+	while (temp)
+	{
+		if (strcmp(key, temp->key) == 0)
+		{
+			free(temp->value);
+			temp->value = (strdup(value));
+			return (1);
+		}
+		temp = temp->next;
+	}
+	temp = ht->array[idx];
+	while (temp)
+	{
+		newNode = malloc(sizeof(hash_node_t));
+		if (newNode == NULL)
+			return (0);
+		ht->array[idx] = newNode;
+		newNode->key = (strdup(key));
+		newNode->value = (strdup(value));
+		newNode->next = temp, ht->array[idx] = newNode;
+		return (1);
+	}
+	return (0);
 }
