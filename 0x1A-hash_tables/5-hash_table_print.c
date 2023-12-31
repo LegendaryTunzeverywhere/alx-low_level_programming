b8a01fcb69f618449e81b8d2c6a8766e84059843
Adewale Okunade Christopher <princecph4@gmail.com>
2023-12-31 00:00:31 +0000

    Task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+* hash_table_print - function
+* @ht: hash table to print
+*
+* Description: function to print a hash table
+* Return: Nothing
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *moveNode;
+	unsigned long int idx;
+	int a = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (idx = 0; index < (ht->size); ++idx)
+	{
+		moveNode = ht->array[idx];
+		while (moveNode != NULL)
+		{
+			if (a > 0)
+				printf(", ");
+
+			printf("\'%s\': \'%s\'", (moveNode->key), (moveNode->value));
+			moveNode = moveNode->next;
+			++a;
+		}
+	}
+	printf("}\n");
+}
